8999c20b8f6479914f916eeeaf0e267e4a2f5fcb
Radenko <tudomov@gmail.com>
2017-06-02 12:58:09 +0200

    Rijesen datum, treba jos statistika

--- app/src/main/java/rtrk/pnrs1/ra38_2014/NewTaskActivity.java
+++ app/src/main/java/rtrk/pnrs1/ra38_2014/NewTaskActivity.java
@@ -9,7 +9,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
@@ -31,7 +30,7 @@ public class NewTaskActivity extends AppCompatActivity {
     final TaskAdapter adapter = MainActivity.getTaskAdapter();
     Task task = new Task(0,0,true,0,0, false, "", "", 0,"",0);
     Calendar danas, taskDate;
-    int dayToday, dayOfTask, dayOfWeek, year, taskYear;
+    int dayToday, dayOfTask, dayOfWeek, year, taskYear, monthOfTask, dayOfMonth, yearOfTask;
     String Datum, nazivLijevogDugmeta, nazivDesnogDugmeta;
     TaskDBHelper dbHelper;
     int pozz;
@@ -112,6 +111,7 @@ public class NewTaskActivity extends AppCompatActivity {
                 zuti.setBackgroundResource(R.color.zuta);
                 zeleni.setBackgroundResource(R.color.zelena);
                 crveni.setBackgroundResource(R.color.tamnoCrvena);
+                prioritet = 1;
 
             }else if(taskk.getmView() == 2){
                 crveni.setEnabled(true);
@@ -120,6 +120,7 @@ public class NewTaskActivity extends AppCompatActivity {
                 zuti.setBackgroundResource(R.color.tamnoZuta);
                 zeleni.setBackgroundResource(R.color.zelena);
                 crveni.setBackgroundResource(R.color.crvena);
+                prioritet = 2;
             }else if(taskk.getmView() == 3){
                 crveni.setEnabled(true);
                 zuti.setEnabled(true);
@@ -127,6 +128,7 @@ public class NewTaskActivity extends AppCompatActivity {
                 zuti.setBackgroundResource(R.color.zuta);
                 zeleni.setBackgroundResource(R.color.tamnoZelena);
                 crveni.setBackgroundResource(R.color.crvena);
+                prioritet = 3;
             }
             dodaj.setEnabled(true);
 
@@ -231,19 +233,25 @@ public class NewTaskActivity extends AppCompatActivity {
                 taskDate = Calendar.getInstance();
 
                 //PROVERA
-                danas.set(2017,11,30);
+                //danas.set(2017,11,30);
 
                 taskDate.set(datum.getYear(),datum.getMonth(),datum.getDayOfMonth());
 
                 dayToday = danas.get(Calendar.DAY_OF_YEAR);
+                year = danas.get(Calendar.YEAR);
+
                 dayOfTask = taskDate.get(Calendar.DAY_OF_YEAR);
                 dayOfWeek = taskDate.get(Calendar.DAY_OF_WEEK);
-                year = danas.get(Calendar.YEAR);
                 taskYear = taskDate.get(Calendar.YEAR);
+                monthOfTask = taskDate.get(Calendar.MONTH);
+                dayOfMonth = taskDate.get(Calendar.DAY_OF_MONTH);
+                yearOfTask = taskDate.get(Calendar.YEAR);
+
 
 
                 Log.i("DAN ZADATKA:",Integer.toString(dayOfTask));
                 Log.i("DAN DANAS:",Integer.toString(dayToday));
+                Log.i("Mjesec ZADATKA: ", Integer.toString(monthOfTask));
 
 
                 if(year == datum.getYear() || (taskYear - year == 1)) {
@@ -307,15 +315,15 @@ public class NewTaskActivity extends AppCompatActivity {
                 task.setmText1(nazivZadatka);
                 task.setmText2(Datum);
                 task.setmCheckBox(false);
-                task.setGodina(taskYear);
-                task.setDan(dayOfTask);
-                task.setMjesec(taskDate.get(Calendar.DAY_OF_YEAR));
+                task.setGodina(yearOfTask);
+                task.setDan(dayOfMonth);
+                task.setMjesec(monthOfTask);
                 task.setSat(vrijeme.getCurrentHour());
                 task.setMinut(vrijeme.getCurrentMinute());
                 task.setmRadioButton(podsjetnik);
                 task.setOpis(opis.getText().toString());
 
-                Task taskk = new Task(dayOfTask, taskYear, false, vrijeme.getCurrentMinute(), taskDate.get(Calendar.MONTH), podsjetnik, nazivZadatka, Datum, prioritet, opis.getText().toString(),vrijeme.getCurrentHour());
+                //Task taskk = new Task(dayOfTask, taskYear, false, vrijeme.getCurrentMinute(), taskDate.get(Calendar.MONTH), podsjetnik, nazivZadatka, Datum, prioritet, opis.getText().toString(),vrijeme.getCurrentHour());
 
                 Intent returnIntent = new Intent();
                 returnIntent.putExtra("result",task);
@@ -323,7 +331,7 @@ public class NewTaskActivity extends AppCompatActivity {
                 finish();
 
                 if(pozz != -1){
-                    dbHelper.updateTask(taskk, pozz);
+                    dbHelper.updateTask(task, pozz);
                 }else{
                     dbHelper.addTask(task);
                 }
